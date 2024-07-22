8062d1894f3947dbb02aa55a3e980f86d6c4a9c2
TienHuy272 <36787257+TienHuy272@users.noreply.github.com>
2024-07-22 16:14:05 +0700

    Implement OpenAPIDefinition

--- section2/accounts/src/main/java/com/hnt/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/hnt/accounts/AccountsApplication.java
@@ -6,6 +6,29 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+/*@ComponentScans({ @ComponentScan("com.hnt.accounts.controller") })
+@EnableJpaRepositories("com.hnt.accounts.repository")
+@EntityScan("com.hnt.accounts.model")*/
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Account ms API Documentation",
+		description = "Account ms REST API Documentation Desc.",
+		version = "v1",
+		contact = @Contact(
+				name = "HNT",
+				email = "hnt@free.com",
+				url = "https://github.com/TienHuy272/springboo-microservice"
+		),
+		license = @License(
+				name = "Apache2.0",
+				url = "https://github.com/TienHuy272/springboo-microservice"
+		)
+	),
+	externalDocs = @ExternalDocumentation(
+		description = "External MS REST API Documentation",
+		url = "http://host:port/swagger-ui/index.html"
+	)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
