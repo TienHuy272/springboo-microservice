5e47c892317cf766f4510d8256e265b9601363aa
HuyNT <tienhuylionel@gmail.com>
2024-07-22 22:02:08 +0700

    remove unused

--- section2/accounts/src/main/java/com/hnt/accounts/exception/GlobalExceptionHandler.java
+++ section2/accounts/src/main/java/com/hnt/accounts/exception/GlobalExceptionHandler.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
