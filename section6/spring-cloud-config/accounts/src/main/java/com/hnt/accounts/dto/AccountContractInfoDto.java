8ba910f5ef22f2c3760ab09e3cac5e281927c910
HuyNT <huynt@kaopiz.com>
2024-07-25 14:19:06 +0700

    add profile to git and refreshing profile on runtime

--- section6/spring-cloud-config/accounts/src/main/java/com/hnt/accounts/dto/AccountContractInfoDto.java
+++ section6/spring-cloud-config/accounts/src/main/java/com/hnt/accounts/dto/AccountContractInfoDto.java
@@ -1,10 +1,17 @@
 package com.hnt.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContractInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountContractInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
