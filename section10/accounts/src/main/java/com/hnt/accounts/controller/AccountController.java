a2b6910ca4f3953f0f8095b7181685e0526104aa
HuyNT <tienhuylionel@gmail.com>
2024-08-03 09:21:56 +0700

    implement ratelimiter on accounts service

--- section10/accounts/src/main/java/com/hnt/accounts/controller/AccountController.java
+++ section10/accounts/src/main/java/com/hnt/accounts/controller/AccountController.java
@@ -6,6 +6,7 @@ import com.hnt.accounts.dto.CustomerDto;
 import com.hnt.accounts.dto.ErrorResponseDto;
 import com.hnt.accounts.dto.ResponseDto;
 import com.hnt.accounts.service.IAccountService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -238,11 +239,18 @@ public class AccountController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(environment.getProperty("JAVA_HOME"));
+                .body("17.01");
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("17.01 from fallback method");
     }
 
     @GetMapping("/contact-info")
