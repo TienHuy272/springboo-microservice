a3ea2678fcf540b1b94ed702994fd83019e7306d
HuyNT <huynt@kaopiz.com>
2024-08-08 16:45:13 +0700

    Implement spring cloud funtion

---
+++ section13/message/src/main/java/com/hnt/message/function/MessageFunctions.java
@@ -0,0 +1,32 @@
+package com.hnt.message.function;
+
+import com.hnt.message.dto.AccountMsgDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+
+@Configuration
+public class MessageFunctions {
+    public static final Logger log = LoggerFactory.getLogger(MessageFunctions.class);
+
+    @Bean
+    public Function<AccountMsgDto, AccountMsgDto> email() {  // <Input, Output>
+        return accountMsgDto -> {
+            log.info("Sending email with details: {}", accountMsgDto.toString());
+            return accountMsgDto;
+        };
+    }
+
+    @Bean
+    public Function<AccountMsgDto, Long> sms() {  // <Input, Output>
+        return accountMsgDto -> {
+            log.info("Sending sms with details: {}", accountMsgDto.toString());
+            return accountMsgDto.accountNumber();
+        };
+    }
+
+}
